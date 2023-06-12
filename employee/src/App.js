7eddefa283d585b54c798562f77f050710e43dc5
anthonynarine <anarine83@gmail.com>
2023-06-12 10:34:56 -0400

    updated mobile nav links to match desktop + moved props.children render and updated styling

--- employee/src/App.js
+++ employee/src/App.js
@@ -1,12 +1,20 @@
 import "./index";
 import Header from "./components/Header";
 import Employees from "./pages/Employees";
+import { Routes, Route } from "react-router-dom";
+import ErrorPage from "./pages/ErrorPage";
+import Customer from "./pages/Customer";
 
 function App() {
   return (
     <>
       <Header>
-        <Employees />
+        <Routes>
+          <Route path="/employees" element={<Employees />} />
+          <Route path="/customer" element={<Customer />} />
+          <Route path="*" element={<ErrorPage />} />
+          {/* path"*" is a catch all route. if no other defined routes matches path"*" will render */}
+        </Routes>
       </Header>
     </>
   );
--- employee/src/pages/Employees.jsx
+++ employee/src/pages/Employees.jsx
@@ -1,12 +1,11 @@
 import Employee from "../components/Employee";
 import { useState } from "react";
 import AddEmployee from "../components/AddEmployee";
-import {v4 as uuidv4} from "uuid" 
+import { v4 as uuidv4 } from "uuid";
 import EditEmployee from "../components/EditEmployee";
 
-
 function Employees() {
-  const [role, setRole] = useState("Physician");
+    
   const [employees, setEmployees] = useState([
     {
       id: 1,
@@ -48,57 +47,59 @@ function Employees() {
 
   //we ill need a way to identify the employee, and the new data we're going to replace the old data with.
   //this function will be called in EditEmployee on form submit. it will take in the employees info.
-  function updateEmployee (id, newName, newRole) {
+  function updateEmployee(id, newName, newRole) {
     console.log("updateEmployee inside app.js");
-    const updatedEmployees = employees.map((employee)=> {
-      if (id === employee.id){
+    const updatedEmployees = employees.map((employee) => {
+      if (id === employee.id) {
         //return updated employee
-        return {...employee, name: newName, role: newRole }
+        return { ...employee, name: newName, role: newRole };
         //spread operator is adding in employee.id, and img
       }
       return employee;
     });
-    setEmployees(updatedEmployees)
-  };
+    setEmployees(updatedEmployees);
+  }
 
   //adding a new employee
-  function newEmployee(name, role, img){
+  function newEmployee(name, role, img) {
     let newEmployee = {
       id: uuidv4(),
       name,
       role,
       img,
-    }
-    setEmployees([...employees, newEmployee])
+    };
+    setEmployees([...employees, newEmployee]);
   }
 
   const showEmployees = true;
 
   return (
     //min-h-screen will give border to entire screen.
-    <div className="App bg-gray-300 min-h-screen ">
+    <div>
       {showEmployees ? (
         <>
-          <div className="flex flex-wrap justify-center pt-2 " >
+          <div className="flex flex-wrap justify-center ">
             {employees.map((employee) => {
               // console.log(employee);
-              //the component EditEmployee is stored in the variable editEmployee which is a render of 
-              //EditEmployee which is rendered in the employee function comp. 
-              const editEmployee = <EditEmployee
-              id={employee.id}
-              name={employee.name}
-              role={employee.role}
-              updateEmployee={updateEmployee}
-              />
+              //the component EditEmployee is stored in the variable editEmployee which is a render of
+              //EditEmployee which is rendered in the employee function comp.
+              const editEmployee = (
+                <EditEmployee
+                  id={employee.id}
+                  name={employee.name}
+                  role={employee.role}
+                  updateEmployee={updateEmployee}
+                />
+              );
               return (
-              <Employee
-                key={employee.id}
-                id={employee.id}
-                name={employee.name}
-                role={employee.role}
-                img={employee.img}
-                editEmployee={editEmployee}
-              />
+                <Employee
+                  key={employee.id}
+                  id={employee.id}
+                  name={employee.name}
+                  role={employee.role}
+                  img={employee.img}
+                  editEmployee={editEmployee}
+                />
               );
             })}
           </div>
@@ -112,4 +113,3 @@ function Employees() {
 }
 
 export default Employees;
-
